a506dadce082bba5d6bb3cc8c2f1957800da52f0
Daye Kim <dykim315@naver.com>
2023-05-21 00:09:00 +0900

    [Feat] #4 - 탭바 초기 구현

---
+++ OMGiOS/Feature/TabBar/CustomTabBarController.swift
@@ -0,0 +1,69 @@
+//
+//  CustomTabBarController.swift
+//  OMGiOS
+//
+//  Created by 김다예 on 2023/05/20.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    // MARK: - Property
+    
+    
+    // MARK: - UI Property
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setViewController()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        hiddenNavigationBar()
+    }
+    
+    // MARK: - Setting
+    
+    private func setStyle() { }
+    
+    private func setViewController() {
+        let homeViewController = HomeViewController()
+//        homeViewController.tabBarItem = UITabBarItem(title: "홈", image: , tag: 0)
+        let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+        homeNavigationController.setNavigationBarHidden(true, animated: true)
+
+        let todayMissionViewController = TodayMissionViewController()
+//        todayMissionViewController.tabBarItem = UITabBarItem(title: "오늘 미션", image: , tag: 0)
+        let todayMissionNavigationController = UINavigationController(rootViewController: todayMissionViewController)
+        todayMissionNavigationController.setNavigationBarHidden(true, animated: true)
+
+        let historyViewController = TodayMissionViewController()
+//        todayMissionViewController.tabBarItem = UITabBarItem(title: "지난 기록", image: , tag: 0)
+        let historyNavigationController = UINavigationController(rootViewController: historyViewController)
+        historyNavigationController.setNavigationBarHidden(true, animated: true)
+
+        viewControllers = [homeNavigationController,
+                           todayMissionNavigationController,
+                           historyNavigationController]
+    }
+    
+    
+    // MARK: - Action Helper
+    
+    
+    // MARK: - Custom Method
+    
+    private func hiddenNavigationBar() {
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+}
