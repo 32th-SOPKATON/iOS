ccf330e0a9865c209ba28cbd6fd0e873e364117c
hyj422 <hyj422@icloud.com>
2023-05-21 04:46:44 +0900

    #31 [Feat] struct, configurecell func 완성

---
+++ OMGiOS/Feature/History/Model/MissionRecord.swift
@@ -0,0 +1,13 @@
+//
+//  MissionRecord.swift
+//  OMGiOS
+//
+//  Created by 홍유정 on 2023/05/21.
+//
+
+import UIKit
+
+struct MissionRecord {
+    let image: UIImage
+    let missionHistory: String
+}
